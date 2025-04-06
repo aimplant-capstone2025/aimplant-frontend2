087af8df5671602f2b16b46c1cedcb154589e174
Papanin Kittawisinpoon <bampapanin2003@gmail.com>
2025-04-06 20:05:25 +0700

    modify cropped image size in implant card

--- src/components/ImplantResultCard.tsx
+++ src/components/ImplantResultCard.tsx
@@ -36,42 +36,44 @@ const ImplantResultCard: React.FC<ImplantResultCardProps> = ({ implant }) => {
 
   return (
     <div className="border border-purple-200 rounded-lg overflow-hidden">
-      <div className={`h-2 ${getStatusIndicatorColor(implant.status)}`}></div>
-      <div className="p-4 grid grid-cols-3 gap-4">
-        <div className="col-span-1">
+      <div className={`h-1.5 ${getStatusIndicatorColor(implant.status)}`}></div>
+      <div className="p-3 grid grid-cols-3 gap-3"> 
+        {/* change layout to horizontal?????? */}
+        <div className="col-span-1 aspect-square">
           <img 
             src={implant.croppedImage} 
             alt={`Implant ${implant.id}`} 
-            className="w-40 h-29 object-cover rounded-md"
+            className="w-full h-full object-contain rounded-md"
           />
         </div>
         <div className="col-span-2">
-          <div className="flex justify-between mb-2">
+          <div className="flex justify-between mb-1.5">
             <h4 className="font-medium">Implant ID: {implant.id}</h4>
             <span className="font-medium">{implant.position}</span>
             <span className={`font-semibold ${getStatusColor(implant.status)}`}>
               {implant.status}
             </span>
           </div>
-          <h5 className="font-medium mb-1">Bone loss</h5>
-          <ul className="space-y-1 text-sm">
+          <h5 className="font-medium mb-1">Bone Loss Analysis</h5>
+          <ul className="space-y-1">
             <li className="flex justify-between">
-              <span>6m:</span>
-              <span>{implant.boneLoss.sixMonths}%</span>
+              <span>% bone loss:</span>
+              <span>{implant.boneLoss.boneloss}%</span>
             </li>
-            <li className="flex justify-between">
+            {/* <li className="flex justify-between">
               <span>2y:</span>
               <span>{implant.boneLoss.twoYears}%</span>
             </li>
             <li className="flex justify-between">
               <span>5y:</span>
               <span>{implant.boneLoss.fiveYears}%</span>
-            </li>
+            </li> */}
           </ul>
         </div>
       </div>
     </div>
   );
 };
+
 
 export default ImplantResultCard;
