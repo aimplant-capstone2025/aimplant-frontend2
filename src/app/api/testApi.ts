98e322156172569c26994e7b6a48b59b465fbb6a
Papanin Kittawisinpoon <bampapanin2003@gmail.com>
2025-04-06 19:40:58 +0700

    modify bone loss display, connect backend

--- src/app/api/testApi.ts
+++ src/app/api/testApi.ts
@@ -23,11 +23,13 @@ export const predictBoneLossAPI = async (imageFile: File): Promise<{ result_img:
       status: 'Mucositis',
     //   status: implant.class,      // "Peri-implantitis", etc.
       boneLoss: {
-        sixMonths: 0,             // If you have this from backend, replace it
-        // twoYears: Math.round(implant.boneloss), // Use boneloss from backend
-        // fiveYears: Math.round(implant.boneloss + 10), // Fake projection for demo
-        twoYears: 15, // Use boneloss from backend
-        fiveYears: 20,
+        // sixMonths: 0,             // If you have this from backend, replace it
+        // // twoYears: Math.round(implant.boneloss), // Use boneloss from backend
+        // // fiveYears: Math.round(implant.boneloss + 10), // Fake projection for demo
+        // twoYears: 15, // Use boneloss from backend
+        // fiveYears: 20,
+        boneloss: Math.round(implant.boneloss),
+        // boneloss: 20,
       },
       croppedImage: `data:image/jpeg;base64,${implant.img}`, // Use base64 image
     }));
