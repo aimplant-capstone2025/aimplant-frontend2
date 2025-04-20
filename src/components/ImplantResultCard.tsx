88013c0439156932480eabfb651da243f38a0e4e
Papanin Kittawisinpoon <bampapanin2003@gmail.com>
2025-04-20 12:57:21 +0700

    change severity level display

--- src/components/ImplantResultCard.tsx
+++ src/components/ImplantResultCard.tsx
@@ -9,28 +9,32 @@ const ImplantResultCard: React.FC<ImplantResultCardProps> = ({ implant }) => {
   // Get status color
   const getStatusColor = (status: string) => {
     switch (status) {
-      case 'Healthy':
+      case 'Healthy (no BL)':
         return 'text-green-600';
-      case 'Mucositis':
-        return 'text-amber-500';
-      case 'Peri-implantitis':
-        return 'text-red-600';
-      default:
-        return 'text-gray-700';
+      case 'Level 1 (< 15%)':
+        return 'text-yellow-500';
+      case 'Level 2 (15% - 25%)':
+        return 'text-orange-500';
+      case 'Level 3 (25% - 50%)':
+        return 'text-orange-700';
+      case 'Level 4 (> 50%)':
+        return 'text-red-700';
     }
   };
 
   // Get status indicator color
   const getStatusIndicatorColor = (status: string) => {
     switch (status) {
-      case 'Healthy':
+      case 'Healthy (no BL)':
         return 'bg-green-500';
-      case 'Mucositis':
-        return 'bg-amber-500';
-      case 'Peri-implantitis':
-        return 'bg-red-500';
-      default:
-        return 'bg-gray-500';
+      case 'Level 1 (< 15%)':
+        return 'bg-yellow-400';
+      case 'Level 2 (15% - 25%)':
+        return 'bg-orange-400';
+      case 'Level 3 (25% - 50%)':
+        return 'bg-orange-600';
+      case 'Level 4 (> 50%)':
+        return 'bg-red-600';
     }
   };
 
@@ -49,25 +53,28 @@ const ImplantResultCard: React.FC<ImplantResultCardProps> = ({ implant }) => {
         <div className="col-span-2">
           <div className="flex justify-between mb-1.5">
             <h4 className="font-medium">Implant ID: {implant.id}</h4>
-            <span className="font-medium">{implant.position}</span>
-            <span className={`font-semibold ${getStatusColor(implant.status)}`}>
+            {/* <span className="font-medium">{implant.position}</span> */}
+            {/* <span className={`font-semibold ${getStatusColor(implant.status)}`}>
+              <p className="text-sm whitespace-pre-line">{implant.status}</p>
+            </span> */}
+          </div>
+          <h5 className="font-medium mb-1">Analysis</h5>
+          <ul className="space-y-1">
+            {/* <li className="flex justify-between">
+              <span>% bone loss:</span>
+              <span>{implant.boneLoss.boneloss}%</span>
+            </li> */}
+            <li className="flex justify-between">
+              <span>severity level:</span>
+              <span className={`font-semibold ${getStatusColor(implant.status)}`}>
+              {/* <p className="text-sm whitespace-pre-line">{implant.status}</p> */}
               {implant.status}
             </span>
-          </div>
-          <h5 className="font-medium mb-1">Bone Loss Analysis</h5>
-          <ul className="space-y-1">
+            </li>
             <li className="flex justify-between">
               <span>% bone loss:</span>
               <span>{implant.boneLoss.boneloss}%</span>
             </li>
-            {/* <li className="flex justify-between">
-              <span>2y:</span>
-              <span>{implant.boneLoss.twoYears}%</span>
-            </li>
-            <li className="flex justify-between">
-              <span>5y:</span>
-              <span>{implant.boneLoss.fiveYears}%</span>
-            </li> */}
           </ul>
         </div>
       </div>
